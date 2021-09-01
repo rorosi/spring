f18d5cd018be2d80552691a863fb89e64b51a59f
rorosi <56014938+rorosi@users.noreply.github.com>
2021-09-01 16:23:00 +0900

    Delete week04 directory

--- week04/src/main/java/com/sparta/week04/models/ProductMypriceRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.week04.models;
-
-import lombok.Getter;
-
-@Getter
-public class ProductMypriceRequestDto {
-    private int myprice;
-}
--- week04/src/main/java/com/sparta/week04/models/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sparta.week04.models;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
